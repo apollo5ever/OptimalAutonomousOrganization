c5ddcffd94f148cbbc6d0286e37c7fcc57a18ed9
apollo5ever <backend@ambrosia.money>
2022-05-08 16:04:50 -0500

    more functionality

--- src/oao.js
+++ src/oao.js
@@ -51,13 +51,26 @@ const OAO = () => {
 		contract.transferSeat(event.target.newOwner.value,event.target.seat.value);
 	}
 	
+	const openVote = (event) => {
+		event.preventDefault();
+		contract.openVote(event.target.action.value,event.target.seatOrAmount.value,event.target.ownerOrAddress.value);
+	}
+	
+	const showHistory = async (event) => {
+		event.preventDefault();
+		//const cars = [];
+		let cars = await contract.votes[0];
+		//let val = contract.votes[event.target.index.value];
+		console.log(JSON.parse(cars));
+	}
+	
 	return (
 		<div>
 			<h1> {"Optimal Autonomous Organization"} </h1>
-			<h3> Transfer Seat </h3>
+			
 			<button onClick = {connectWalletHandler}> {connButtonText}</button>
 			<h3> Address : {defaultAccount} </h3>
-			
+			<h3> Transfer Seat </h3>
 			<form onSubmit={transferSeat}>
 				<p> New Owner </p>
 				<input id='newOwner' type='text' />
@@ -66,6 +79,23 @@ const OAO = () => {
 				<button type={"submit"}> Transfer Seat </button>
 			</form>
 			
+			<h3> Open Vote </h3>
+			<form onSubmit={openVote}>
+			<p> Action </p>
+			<input id='action' type='text' />
+			<p> Seat or Amount </p>
+			<input id='seatOrAmount' type='text' />
+			<p> Owner or Address </p>
+			<input id='ownerOrAddress' type='text' />
+			<button type={"submit"}> Open Vote </button>
+			</form>
+			
+			<h3> Show Vote History </h3>
+			<form onSubmit={showHistory}>
+			<p>Index </p>
+			<input id='index' type='text' />
+			<button type={"submit"}> Show Vote </button>
+			</form>
 
 			{errorMessage}
 		</div>
